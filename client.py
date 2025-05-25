1c89e9839b0a18aa5c90e59973a039941599decb
Zendixx55 <amit1921997@gmail.com>
2025-05-25 16:05:46 +0300

    Update client.py
    Cleaned the script, and added explanatory comment

--- client.py
+++ client.py
@@ -1,17 +1,11 @@
 import socket
-import threading
 import time
 
 
-# def listen_for_messages():
-#     messages = client_socket.recv(1024).decode()
-#     print(messages)
 
 client_socket = socket.socket()
-client_socket.connect(('127.0.0.1' , 1337))
-# command = input('Terminal: ')
-# t1 = threading.Thread(target=listen_for_messages)
-# t1.start()
+client_socket.connect(('127.0.0.1' , 1337)) # Change to the servers IP address
+
 while True:
     messages = client_socket.recv(1024).decode()
     print(messages)
