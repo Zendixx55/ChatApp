207efca5c89a5469267da779457f4965302ff5c0
Zendixx55 <amit1921997@gmail.com>
2025-05-27 18:12:00 +0300

    change the script to make it receive and send messages at the same time with threads

--- client.py
+++ client.py
@@ -1,15 +1,23 @@
 import socket
-import time
+import threading
 
+def listen_to_messages():
+    while True:
+        messages = client_socket.recv(1024).decode()
+        print(messages)
 
+def send_message():
+    while True:
+        command = input('> ')
+        client_socket.send(command.encode())
 
 client_socket = socket.socket()
 client_socket.connect(('127.0.0.1' , 1337)) # Change to the servers IP address
 
-while True:
-    messages = client_socket.recv(1024).decode()
-    print(messages)
-    command = input('Your message: ')
-    client_socket.send(command.encode())
+listening_thread = threading.Thread(target=listen_to_messages)
+sending_thread = threading.Thread(target=send_message)
+
+listening_thread.start()
+sending_thread.start()
 
 
